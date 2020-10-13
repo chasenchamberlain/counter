a1bdbd931cd2b17709aee4aa0bd4e9f340bb859c
chasenchamberlain <chasen.chamberlain@gmail.com>
2020-10-12 20:20:01 -0700

    ready to pass stuff to main

--- app/js/index.js
+++ app/js/index.js
@@ -3,6 +3,9 @@ var b = require('bootstrap');
 const { nodeName } = require('jquery');
 const ipcRenderer = require('electron').ipcRenderer;
 var games = [];
+
+// this is what gives our cloned form inputs correct ids
+var inputCount = 0;
 // export for others scripts to use
 // window.$ = $;
 // window.jQuery = jQuery;
@@ -25,17 +28,19 @@ function sendForm(event) {
     // var testArray = [{name: "sm64", count: 0}, {name: "wow", count: 666}, ];
 
     // grab the goodies??
-    var formCount = $('div.form-row').length - 1;
+    // var formCount = $('div.form-row').length - 1;
+    for (var i = 0; i <= inputCount; i++) {
+        let countInputTitle = document.getElementById("countTitle" + i).value;
+        let countInputCount = document.getElementById("countInput" + i).value;
 
-    for (var i = 0; i < formCount; i++) {
-
+        var count = { [countInputTitle]: countInputCount };
+        games.push(count);
     }
-
-    let gameCount = document.getElementById("countInput").value;
-    ipcRenderer.send('form-submission', gameName, gameCount)
+    console.log({ games });
+    // let gameCount = document.getElementById("countInput").value;
+    // ipcRenderer.send('form-submission', gameName, gameCount)
 }
 
-var inputCount = 0;
 function addAnotherFormInput() {
     var original = document.getElementById('countInputDiv0');
     var clone = original.cloneNode(true);
@@ -45,8 +50,8 @@ function addAnotherFormInput() {
     // Testing weird things.
     let kids = clone.childNodes;
     console.log("NUMBER===" + inputCount)
-    $(clone).find("#countTitle").attr("id", 'countTitle' + inputCount);
-    $(clone).find("#countInput").attr("id", 'countInput' + inputCount);
+    $(clone).find("#countTitle0").attr("id", 'countTitle' + inputCount);
+    $(clone).find("#countInput0").attr("id", 'countInput' + inputCount);
     original.parentNode.appendChild(clone);
     console.log("-------");
     console.log(kids[1]);
